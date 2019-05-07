796a50d92643ddd179d0d0fc97d1f025822969d3
Muthiah Prabhakaran <muthiahprabhakaran@gmail.com>
2019-05-07 13:26:24 +0530

    made as thread safe

--- creational-design-patterns/src/main/java/com/dp/cdp/singlton/DbSingleton.java
+++ creational-design-patterns/src/main/java/com/dp/cdp/singlton/DbSingleton.java
@@ -1,15 +1,23 @@
 package com.dp.cdp.singlton;
 
 public class DbSingleton {
-	
-	private  static DbSingleton instance = null;
-	
+
+	private static volatile DbSingleton instance = null; // volatile will protect even there is change in JVM
+
 	private DbSingleton() {
+		if (instance != null) {
+			throw new RuntimeException("use getInstance() method to create");
+		}
 	}
-	
+
 	public static DbSingleton getInstance() {
-		if(instance == null) {
-			instance = new DbSingleton();
+		if (instance == null) {
+			synchronized (DbSingleton.class) { // it will protect from objects created through reflection + if two
+												// threads concurrently trying to create an instance
+				if (instance == null) {
+					instance = new DbSingleton();
+				}
+			}
 		}
 		return instance;
 	}
