d4e103f242f0cc3e409a0f10782154770eaf4149
Muthiah Prabhakaran <muthiahprabhakaran@gmail.com>
2019-05-14 12:56:33 +0530

    Implemented connection in singleton pattern

--- creational-design-patterns/src/main/java/com/dp/cdp/singlton/DbSingleton.java
+++ creational-design-patterns/src/main/java/com/dp/cdp/singlton/DbSingleton.java
@@ -1,24 +1,59 @@
 package com.dp.cdp.singlton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSingleton {
 
-	private static volatile DbSingleton instance = null; // volatile will protect even there is change in JVM
+    private static volatile DbSingleton instance = null; // volatile will protect even there is
+                                                         // change in JVM
+    private static volatile Connection conn = null;
 
-	private DbSingleton() {
-		if (instance != null) {
-			throw new RuntimeException("use getInstance() method to create");
-		}
-	}
+    private DbSingleton() {
 
-	public static DbSingleton getInstance() {
-		if (instance == null) {
-			synchronized (DbSingleton.class) { // it will protect from objects created through reflection + if two
-												// threads concurrently trying to create an instance
-				if (instance == null) {
-					instance = new DbSingleton();
-				}
-			}
-		}
-		return instance;
-	}
+        try {
+            // Driver implementation
+            // DriverManager.registerDriver(new org.apache.derby.jdbc.EmbedderDriver());
+        } catch (Exception e) { // Change it to SqlException
+            e.printStackTrace();
+        }
+
+        if (conn != null) {
+            throw new RuntimeException("use getConnection() method to create");
+        }
+
+        if (instance != null) {
+            throw new RuntimeException("use getInstance() method to create");
+        }
+    }
+
+    public static DbSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DbSingleton.class) { // it will protect from objects created through
+                                               // reflection + if two
+                                               // threads concurrently trying to create an instance
+                if (instance == null) {
+                    instance = new DbSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection() { // no-static
+        if (conn == null) {
+            synchronized (DbSingleton.class) {
+                if (conn == null) {
+                    try {
+                        String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+                        conn = DriverManager.getConnection(dbUrl);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return conn;
+    }
 }
--- creational-design-patterns/src/main/java/com/dp/cdp/singlton/DbSingletonDemo.java
+++ creational-design-patterns/src/main/java/com/dp/cdp/singlton/DbSingletonDemo.java
@@ -1,16 +1,23 @@
 package com.dp.cdp.singlton;
 
+import java.sql.Connection;
+
 public class DbSingletonDemo {
 
 	public static void main(String[] args) {
 
 		DbSingleton instance = DbSingleton.getInstance();
-		System.out.println(instance);
+		/*System.out.println(instance);
 
 		DbSingleton anotherInstance = DbSingleton.getInstance();
 		System.out.println(anotherInstance); // both instance and another instance will be the same object sincle
 												// Singleton allows to create one object
-
+*/
+		Connection conn = instance.getConnection();
+		
+		/**
+		 * logic with conn.createStatement()
+		 */
 	}
 
 }
